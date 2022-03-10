51637c8752d2e91a23bbb28cb541781874445a8e
LeonardoRyuta <leonardo.ryuta05@gmail.com>
2022-03-10 15:52:41 +0000

    deleted stuff in next config

--- pixieland/next.config.js
+++ pixieland/next.config.js
@@ -1,7 +1,3 @@
 module.exports = {
   reactStrictMode: true,
-  images: {
-    loader: 'miniLogo',
-    path: 'https://cdn.discordapp.com/attachments/935249183575789606/951254106385813574/pl_logo_test.png',
-  },
 }
