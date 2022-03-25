19d4cffac3658d0d532f21acd98305d4d4bcd920
LeonardoRyuta <leonardo.ryuta05@gmail.com>
2022-03-25 22:57:15 +0000

    Update hello.js

--- pixieland/pages/api/hello.js
+++ pixieland/pages/api/hello.js
@@ -5,18 +5,5 @@ const bot = new Discord.Client();
 
 
 export default function handler(req, res) {
-  bot.on('ready', () => {
-    console.log(`Logged in as ${bot.user.tag}!`);
-    eventListener()
-    bot.user.setPresence({
-        status: "online", 
-    });
-    bot.user.setActivity(`Afonso`, {
-      type: "STREAMING",
-  });
-  });
-  
-  bot.login('OTU0MzE2MjIxNDY4MjUwMTQz.YjRWPQ.Ms9ISZGTmPwSxCF41y6HlEkd9tY');
-
   res.status(200).json({ name: 'John Doe' })
 }
