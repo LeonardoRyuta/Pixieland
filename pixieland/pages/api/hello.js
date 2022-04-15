bb9354b2f8a8cd552beebe551edec82293cab173
LeonardoRyuta <leonardo.ryuta05@gmail.com>
2022-04-15 19:38:30 +0100

    Update hello.js

--- pixieland/pages/api/hello.js
+++ pixieland/pages/api/hello.js
@@ -1,8 +1,4 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-const Discord = require('discord.js')
-const { RichEmbed } = require("discord.js")
-const bot = new Discord.Client();
-
 
 export default function handler(req, res) {
   res.status(200).json({ name: 'John Doe' })
